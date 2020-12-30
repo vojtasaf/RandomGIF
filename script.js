f646d1d06cc1adc01c336997b7a1020b60f033d2
Vojta Šafránek <vojta@bankify.io>
2020-12-30 16:07:11 +0100

    Added button and stylesheet

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-var request = new XMLHttpRequest();
+function generateGif() {
+	var request = new XMLHttpRequest();
 
-var key = config.apiKey;
+	var key = config.apiKey;
 
-request.open("GET", "https://api.giphy.com/v1/gifs/random?api_key=" + key);
+	request.open("GET", "https://api.giphy.com/v1/gifs/random?api_key=" + key);
 
-request.onload = function () {
-	var response = request.response;
-	var parsedData = JSON.parse(response);
-	console.log(parsedData);
+	request.onload = function () {
+		var response = request.response;
+		var parsedData = JSON.parse(response);
+		console.log(parsedData);
 
-	var originalUrl = parsedData.data.images.original.url;
-	console.log(originalUrl);
+		var originalUrl = parsedData.data.images.original.url;
+		console.log(originalUrl);
 
-	var gif = document.createElement("img");
-	gif.setAttribute("src", originalUrl);
-	document.body.appendChild(gif);
-};
+		var gif = document.getElementById("gif");
+		gif.setAttribute("src", originalUrl);
+	};
 
-request.send();
+	request.send();
+}
